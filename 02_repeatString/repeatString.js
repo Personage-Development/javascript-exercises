8c92ebb15ed631e90b9f992e9fed87508aba3614
Personage <noahlines2099@outlook.com>
2024-11-03 05:48:16 -0700

    fulfilled the project requisites

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
+    let final_string = "";
 
+    if(number >= 0) {
+
+        for(let i = 0; i < number; i++) {
+            final_string += word;
+        }
+        return final_string;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
