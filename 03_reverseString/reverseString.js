996c10338e809f43c99a084f2c2b9d2102632b89
Personage <noahlines2099@outlook.com>
2024-11-03 06:43:13 -0700

    finished function to reverse the string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
+const reverseString = function(word) {
+    let length = word.length;
+    flipped_word = "";
+    for(i = 0; i < length; i++) {
+        char = word.slice(length - i - 1, length - i);
+        flipped_word += char;
+    }
+    return flipped_word;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
