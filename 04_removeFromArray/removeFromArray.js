79eeb95caabceeb1d6779e93cca8b10ee0850eec
Personage <noahlines2099@outlook.com>
2024-11-04 04:30:59 -0700

    created the function to remove array elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
 const removeFromArray = function() {
+    let numbers = arguments[0];
+    for(i = 1; i < arguments.length; i++) {
+        console.log(i);
+        let numbers1 = [];
+        for (let j = 0; j < numbers.length; j++) {
+            console.log(j);  
+            if (numbers[j] !== arguments[i]) {
+                numbers1.push(numbers[j]);
+            }
+        }
+        numbers = numbers1;
+    }
+    return numbers;
 };
 
 // Do not edit below this line
