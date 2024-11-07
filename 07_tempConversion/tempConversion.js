6746bebd66cd36de63d83c8e66c442dbe072cf59
Personage <noahlines2099@outlook.com>
2024-11-07 01:21:19 -0700

    finished functions to convert temps rounded

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let converted_temp = (temp -32) * (5/9);
+  let roundedNumber = Math.round(converted_temp * 10) / 10;
+  return roundedNumber;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let converted_temp = (temp * (9/5)) + 32;
+  let roundedNumber = Math.round(converted_temp * 10) / 10;
+  return roundedNumber;
 };
 
 // Do not edit below this line
